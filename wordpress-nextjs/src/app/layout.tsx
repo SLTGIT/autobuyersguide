39f582931264ec2e9630e8b91d9376d4eb4f282a
aditya <aditya@slt.work>
2026-04-28 17:56:15 +0530

    implement the Google Analytics (GA) key

--- wordpress-nextjs/src/app/layout.tsx
+++ wordpress-nextjs/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "@/styles/globals.scss";
@@ -18,6 +19,7 @@ const siteOrigin = rawSite
 const title = "Quality Used Cars Brisbane | Expert Finance &amp; Sourcing";
 const description =
   "Access premium used 4x4s, SUVs, and commercial vehicles. Based in Ormiston, we provide $0 deposit finance and statewide delivery from Brisbane to Cairns.";
+const GTM_ID = "GTM-W397LKXC";
 
 export const metadata: Metadata = {
   metadataBase: new URL(siteOrigin),
@@ -54,6 +56,23 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={manrope.className}>
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${GTM_ID}');
+          `}
+        </Script>
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <ClientImports />
         <Layout>{children}</Layout>
       </body>
