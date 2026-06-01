fe6229ef9953a341723848dbd7ca77e55ddc5ebf
aditya <aditya@slt.work>
2026-06-01 14:33:53 +0530

    final testing for changes

--- wordpress-nextjs/src/components/layout/Footer.tsx
+++ wordpress-nextjs/src/components/layout/Footer.tsx
@@ -45,9 +45,9 @@ export default function Footer() {
               <li>
                 <Link href="/about-us">About Us</Link>
               </li>
-              {/* <li>
+              <li>
                 <Link href="/contact">Contact Us</Link>
-              </li> */}
+              </li>
               <li>
                 <Link href="/privacy-policy">Privacy Policy</Link>
               </li>
